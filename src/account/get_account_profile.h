07a0f07d317461a23608247b3b274e23f46867e1
perniciousflier <perniciousflier@gmail.com>
2024-11-28 14:14:00 +0100

    buf fix on get_account_profile.h

--- src/account/get_account_profile.h
+++ src/account/get_account_profile.h
@@ -45,6 +45,30 @@ public:
 
 // Function: GetAccountProfile.get
 
+//     Prototype:
+///@param         char* getEPurl();
+
+//     Parameters:
+       
+//     Returns:
+///@returns         char* sme_httpsUrl + endpoint
+
+    char* getEPurl();
+
+// Function: BucketOpen.getWSEPurl
+
+//     provides this endpoint
+
+//     Prototype:
+///@param         char* getWSEPurl();
+
+//     Parameters:
+       
+//     Returns:
+///@returns         char* endpoint
+
+    char* getWSEPurl();
+    
 //     return the json script
 
 //     Prototype:
